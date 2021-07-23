869253c0403e45dbe26209fd0da0a9172a33444e
bongja <qkr911125@naver.com>
2021-07-23 22:38:36 +0900

    no message

---
+++ Member_Study4/src/main/java/com/study/controller/MemberController.java
@@ -0,0 +1,52 @@
+package com.study.controller;
+
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.study.domain.MemberVO;
+import com.study.service.MemberService;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class MemberController {
+	
+	@Setter(onMethod_ = @Autowired)
+	private MemberService service;
+	
+	@GetMapping("/join")
+	public String join(@ModelAttribute MemberVO vo) {
+		return "/join";
+		}
+	
+	@PostMapping("/join")
+	public String joinPost(@Valid MemberVO vo, BindingResult BindingResult) {
+		if( BindingResult.hasErrors() ) {
+
+			// 에러를 List로 저장
+			List<ObjectError> list = BindingResult.getAllErrors();
+			for( ObjectError error : list ) {
+				System.out.println(error);
+			}
+			return "/join";
+		}
+		service.insertMember(vo);
+		return "redirect:/joinmember";
+	}
+	
+	@GetMapping("/joinmember")
+	public void joinmember() {}
+}
---
+++ Member_Study4/src/main/java/com/study/domain/MemberVO.java
@@ -0,0 +1,26 @@
+package com.study.domain;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+	
+	@NotEmpty(message="이메일을 입력하세요.")
+	  @Email
+	private String mem_id;
+	
+	@Pattern(regexp="(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])(?=\\S+$).{8,}")
+	private String mem_pw;
+	
+	@NotEmpty(message="이름을 정확히 입력하세요..")
+	private String mem_name;
+	
+	@Pattern(message="휴대폰 번호를 정확히 입력하세요.", regexp = "^01(?:0|1|[6-9]) - (?:\\d{3}|\\d{4}) - \\d{4}$")
+	private String mem_phone;
+}
