af0633f14af6de897e68b03a7b24d6fcdce4016a
bongja <qkr911125@naver.com>
2021-08-05 22:12:20 +0900

    no message

--- Moviemoving2/src/main/java/com/Moviemoving/controller/BookmarkController.java
+++ Moviemoving2/src/main/java/com/Moviemoving/controller/BookmarkController.java
@@ -1,5 +1,27 @@
 package com.Moviemoving.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.Moviemoving.service.BookmarkService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/Bookmark/*")
+@AllArgsConstructor
 public class BookmarkController {
+	private BookmarkService service; //자동주입
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		log.info("....list");
+		model.addAttribute("list",service.insertBookmark());
+	}
 
 }
+
