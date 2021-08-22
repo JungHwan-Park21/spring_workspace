69313c3715eff5cb65dab03c2f1f4a1361f98711
bongja <qkr911125@naver.com>
2021-08-22 16:23:10 +0900

    no message

--- Moviemoving_All2/src/main/java/com/Moviemoving/mapper/MovieRateMapper.java
+++ Moviemoving_All2/src/main/java/com/Moviemoving/mapper/MovieRateMapper.java
@@ -1,5 +1,24 @@
 package com.Moviemoving.mapper;
 
-public interface MovieRateMapper {
+import java.util.List;
 
+import com.Moviemoving.domain.MovieRateVO;
+import com.Moviemoving.domain.MovieVO;
+
+public interface MovieRateMapper {
+		//평가 리스트
+		public List<MovieVO> movie_bookmark(String User_id);
+		public List<MovieVO> tv_bookmark(String User_id);
+		
+		//평가 insert
+		public void insertBookmark(MovieRateVO movieRate);
+		
+		//평가 select
+		public MovieRateVO read(Long bno); 
+		
+		//평가 delete
+		public void deleteBookmark(MovieRateVO movieRate);
+		
+		//평가 update
+		public void update(MovieRateVO movieRate);
 }
