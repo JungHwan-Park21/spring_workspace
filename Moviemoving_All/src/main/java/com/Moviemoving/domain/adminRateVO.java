88f82248cd2ce7895d8c3529c3d70b3ca4d48224
bongja <qkr911125@naver.com>
2021-08-25 23:19:20 +0900

    no message

---
+++ Moviemoving_All/src/main/java/com/Moviemoving/domain/adminRateVO.java
@@ -0,0 +1,12 @@
+package com.Moviemoving.domain;
+
+import lombok.Data;
+
+@Data
+public class adminRateVO {
+	private String MovieInfo_no;
+	private String User_id;
+	private String MovieRate_score;
+	private String MovieRate_con;
+	
+}
